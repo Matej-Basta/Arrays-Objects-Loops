ef674be147f60d7e5a7e1a6341e1db51ca01bb75
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-27 21:02:43 +0100

    Finished exercise

--- script.js
+++ script.js
@@ -2,11 +2,64 @@ const images = Array.from(document.querySelectorAll("img"));
 
 console.log(typeof images);
 
-let srcOfImages = [];
+let alts = [];
+let urls = [];
 
 images.forEach((element) => {
   console.log(element.getAttribute("src"));
-  srcOfImages.push(element.getAttribute("src"));
+  urls.push(element.getAttribute("src"));
+
+  console.log(element.getAttribute("alt"));
+  alts.push(element.getAttribute("alt"));
 });
 
-console.log(srcOfImages);
+console.log(urls);
+console.log(alts);
+
+console.log(images.length);
+
+const shallowCopy = urls.slice(0, 3);
+console.log(shallowCopy);
+
+let first = [];
+
+for (let i = 0; i < urls.length; i++) {
+  first.push(urls[i]);
+
+  if (i === 2) {
+    break;
+  }
+}
+
+console.log(first);
+
+const last3Elements = urls.slice(urls.length - 3);
+console.log(last3Elements);
+
+let last = [];
+
+for (let i = 0; i < urls.length; i++) {
+  if (i < urls.length - 3) {
+    continue;
+  }
+  last.push(urls[i]);
+}
+
+console.log(last);
+
+const object = {
+  urls: urls,
+  alts: alts,
+};
+
+console.log(object);
+
+const arrayWithCatInside = alts.filter((element) => element.match(/cat/i));
+
+console.log(arrayWithCatInside);
+
+const ul = document.querySelector(".list-of-photos__list");
+
+alts.forEach((element) => {
+  ul.innerHTML += "<li>" + element + "</li>";
+});
