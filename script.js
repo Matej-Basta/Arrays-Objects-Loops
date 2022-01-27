b633fae22c8cd6d4de047e7b0cab2e60227b5bac
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-27 20:26:45 +0100

    Step 2 - done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const images = document.querySelectorAll("img");
+const images = Array.from(document.querySelectorAll("img"));
 
-console.log(images);
+console.log(typeof images);
+
+let srcOfImages = [];
+
+images.forEach((element) => {
+  console.log(element.getAttribute("src"));
+  srcOfImages.push(element.getAttribute("src"));
+});
+
+console.log(srcOfImages);
